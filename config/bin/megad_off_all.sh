60b9cab9acef2d89191ccfa810dea266662052a8
Maksim Lysenka <glifery@gmail.com>
2021-08-13 01:09:06 +0300

    Master switch and motion sensor

---
+++ config/bin/megad_off_all.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+/usr/bin/curl "http://192.168.0.141/sec/?cmd=8:0;9:0;10:0;12:0;p50;7:0"
+/usr/bin/curl "http://192.168.0.141/sec/?cmd=13:0;22:0;23:0;25:0;27:0;28:0"
+/usr/bin/curl "http://192.168.0.142/sec/?cmd=a:0"
